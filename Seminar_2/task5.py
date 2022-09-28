1976b78196d9ab1c9b61f7030c6e3bef08446038
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-09-28 20:41:00 +0700

    Python Seminar2 Task5 HomeWork

---
+++ Seminar_2/task5.py
@@ -0,0 +1,21 @@
+# Реализуйте алгоритм перемешивания списка. Без функции shuffle из модуля random.
+
+from random import random, randrange
+from tkinter import N
+
+
+number = int(input('Введите количество элементов списка: '))
+basis = []
+result = []
+index = 0
+
+for i in range(number):
+    basis.append(i)
+print(basis)    
+    
+while (number != 0):
+    index = randrange(number)
+    result.append(basis[index])
+    basis.remove(basis[index])
+    number-=1
+print(result)

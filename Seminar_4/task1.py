6da093894cbbb0976122bbb985cfec0e1d05cc85
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-10-05 10:56:10 +0700

    Python Seminar4 Task1 HomeWork

--- Seminar_4/task1.py
+++ Seminar_4/task1.py
@@ -3,7 +3,7 @@
 number = float(input('Введите число: '))
 
 def set_accuracy(num):
-    accuracy = int(input('Введите количество знаков после запятой: '))
+    accuracy = int(input('Введите количество знаков после запятой (точность): '))
     print(format(num, f'0.{accuracy}f'))
     return (format(num, f'0.{accuracy}f'))
 

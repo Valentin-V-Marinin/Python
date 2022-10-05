783f7f3dd455ffd13cd627aeb98d1241d42d749a
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-10-05 10:52:44 +0700

    Python Seminar4 Task1 HmeWork

---
+++ Seminar_4/task1.py
@@ -0,0 +1,10 @@
+# Вычислить число c заданной точностью d
+
+number = float(input('Введите число: '))
+
+def set_accuracy(num):
+    accuracy = int(input('Введите количество знаков после запятой: '))
+    print(format(num, f'0.{accuracy}f'))
+    return (format(num, f'0.{accuracy}f'))
+
+set_accuracy(number)

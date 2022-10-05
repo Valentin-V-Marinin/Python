c4af8cceeaad3822e8ba644a3d246bd6e0cd2bd3
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-10-05 16:33:30 +0700

    Python Seminar4 Task1 HomeWork

--- Seminar_4/task1.py
+++ Seminar_4/task1.py
@@ -1,10 +1,18 @@
 # Вычислить число c заданной точностью d
 
-number = float(input('Введите число: '))
+from decimal import Decimal
 
-def set_accuracy(num):
-    accuracy = int(input('Введите количество знаков после запятой (точность): '))
+number = Decimal(input('Введите число: '))
+accuracy = int(input('Введите количество знаков после запятой (точность): '))
+
+
+def set_accuracy(num, accuracy):
     print(format(num, f'0.{accuracy}f'))
     return (format(num, f'0.{accuracy}f'))
 
-set_accuracy(number)
+def alter_accuracy(num: Decimal, accuracy):
+    print(num.quantize(Decimal(f"0.{10**(accuracy-1)}")))
+    return num.quantize(Decimal(f"0.{10**(accuracy-1)}"))
+
+set_accuracy(number, accuracy)
+alter_accuracy(number, accuracy)

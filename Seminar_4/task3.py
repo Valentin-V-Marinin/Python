52164da160a15ad4049d08400b582a44bf9c6bdf
Valentin V. Marinin <v_v_m_70@mail.ru>
2022-10-06 14:10:14 +0700

    Python Seminar4 Task3 HomeWork

---
+++ Seminar_4/task3.py
@@ -0,0 +1,24 @@
+# Задайте последовательность чисел. Напишите программу, которая 
+#  выведет список неповторяющихся элементов исходной последовательности 
+#  в том же порядке.
+
+from random import randrange
+
+number = int(input('Введите количество элементов списка: '))
+
+def random_list(numb):
+    result = []
+    for i in range(numb): 
+        result.append(randrange(0,numb))
+    return result    
+
+def result_list(mylist : list):
+    result = []
+    for i in range(len(mylist)):
+        if (mylist.count(mylist[i]) == 1): result.append(mylist[i])
+    return result
+
+spisok = random_list(number)
+print(spisok)
+print(result_list(spisok)) 
+ 
